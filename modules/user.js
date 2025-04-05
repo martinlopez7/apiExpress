284c86650d854798ced8d0606d49e08dd5f4ebf1
martin <martin.lopez@alumnos.uneatlantico.es>
2025-04-05 14:03:54 +0200

    hash de password y metodo para comprobar contrasñas

--- modules/user.js
+++ modules/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -16,6 +17,24 @@ const userSchema = new mongoose.Schema({
         default: Date.now
     }
 });
+
+// Middleware para hashear la contraseña antes de guardar
+userSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+    
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+// Método para comparar contraseñas
+userSchema.methods.comparePassword = async function(candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
